9aba2644223c9c26f362ad62e2a9faa76754a82f
arunamanoharan <aruna.vish17@gmail.com>
2021-02-07 13:02:11 +0530

    data

---
+++ src/org/javaProj/test.java
@@ -0,0 +1,14 @@
+package org.javaProj;
+
+public class test {
+
+	private void test1() {
+		
+		System.out.println ("tested");
+	}
+private void test() {
+		
+		System.out.println ("tested");
+	}
+	
+}
